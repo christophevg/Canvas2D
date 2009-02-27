f663124a9b78c4388ef5b22aad2d7e882d34ca16
Koen Hautekeete <koen@thesoftwarefactory.be>
2009-02-27 16:06:30 +0100

    Added base class for WebKitCanvas and IECanvas

--- src/IECanvas.js
+++ src/IECanvas.js
@@ -1,11 +1,11 @@
-Canvas2D.WebKitCanvas = Class.create( Canvas2D.CanvasBase, {
+Canvas2D.IECanvas = Class.create( Canvas2D.CanvasBase, {
 
-    fillText     : function(text, x, y, maxWidth) {
+	fillText     : function(text, x, y, maxWidth) {
 		this.strokeText(text, x, y, maxWidth);
 		this.fill();
     },
     
-	strokeText   : function(text, x, y, maxWidth) {
+    strokeText   : function(text, x, y, maxWidth) {
 		this.save();
 		this.lineStyle = "solid";
 		this.lineWidth = 1;
@@ -15,8 +15,8 @@ Canvas2D.WebKitCanvas = Class.create( Canvas2D.CanvasBase, {
     },
     
     measureText  : function(text) {
-    	return CanvasTextFunctions.measure(
-    			this.font, getFontSize(this.font), text);
+		return CanvasTextFunctions.measure(
+				this.font, getFontSize(this.font), text);
     }
 
 } );
