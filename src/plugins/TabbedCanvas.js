2d1fb7cde51a5c82637f666560179940af868403
Christophe VG <xtof@pumpkin.local>
2009-03-02 23:26:54 +0100

    fixed regression in TabbedCanvas plugin and added regression test

--- src/plugins/TabbedCanvas.js
+++ src/plugins/TabbedCanvas.js
@@ -24,7 +24,7 @@ Canvas2D.Book.plugins.TabbedCanvas = Class.create( {
 	about.style.width = (parseInt(width)-4)  + "px";
 
 	var libraries = "";
-	this.book.libraries.each(function(library) {
+	this.book.extensions.each(function(library) {
 	    libraries += "\n<hr>\n";
 	    libraries += "<b>Library: " + library.name + "</b> " + 
 		"by " + library.author + "<br>" +
