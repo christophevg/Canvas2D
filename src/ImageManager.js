967b7dac762f3c06442ff7dfe10b9c31d209c62f
Koen Hautekeete <koen@thesoftwarefactory.be>
2009-04-03 20:45:13 +0200

    Partially fixes bug 193 : fixes image rendering on IE
    image.onLoad should be defined before image.src is set. Also see http://www.thefutureoftheweb.com/blog/image-onload-isnt-being-called.

--- src/ImageManager.js
+++ src/ImageManager.js
@@ -5,11 +5,11 @@ Canvas2D.ImageManager.work = 0;
 Canvas2D.ImageManager.load = function(src, onload) {
     var image = new Image();
     Canvas2D.ImageManager.work++;
-    image.src = src;
     image.onload = function() {
 	Canvas2D.ImageManager.work--;
 	onload();
     }
+    image.src = src;
     return image;
 };
 
