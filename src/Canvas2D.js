c613358c2d835b08a3863999cdab83bb9c2d01d8
Christophe VG <xtof@pumpkin.local>
2009-04-03 16:34:56 +0200

    added convenience method on Canvas2D, wrapping creating of a manager and setting up a sheet added default canvas api to mdc example

--- src/Canvas2D.js
+++ src/Canvas2D.js
@@ -3,3 +3,16 @@ var Canvas2D = {};
 
 // global placeholder for extensions to register
 Canvas2D.extensions = [];
+
+// one-shot activation of a Canvas
+Canvas2D.activate = function(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    if(canvas) {
+	var manager = new Canvas2D.Manager();
+	var canvas  = manager.setupBook(canvasId);
+	var sheet   = canvas.addSheet();
+	manager.startAll();
+	return sheet;
+    }
+    throw( canvasId + " does not reference a known id on the document." );
+};
