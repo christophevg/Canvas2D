d048bffa5edb78ad577e0961cad743b1307f63b4
Christophe VG <xtof@pumpkin.local>
2009-04-28 23:56:47 +0200

    Manager now offers interface for retrieving managed books

--- src/Manager.js
+++ src/Manager.js
@@ -1,25 +1,29 @@
 Canvas2D.Manager = Class.create( {
     initialize : function() {
 	this.plugins = [];
-	this.books   = [];
+	this.books   = $H();
     },
 
     addBook : function(book) {
 	unless( book instanceof Canvas2D.Book, function() { 
 	    throw( "Manager::addBook: book must be instance of Canvas2D.Book" );
 	} );
-	this.books.push(book);
+	this.books.set(book.name, book);
 	return book;
     },
 
     setupBook : function(id) {
 	return this.addBook(new Canvas2D.Book(id));
     },
+
+    getBook : function(id) {
+	return this.books[id];
+    },
     
     startAll :function() {
 	if( Canvas2D.ImageManager.isReady() ) {
-	    this.plugins.each(function(plugin) { plugin.start(); } );
-	    this.books  .each(function(book)   { book  .start(); } );
+	    this.plugins.each(function(plugin) { plugin.start();     } );
+	    this.books  .each(function(book)   { book.value.start(); } );
 	} else {
 	    this.startAll.bind(this).delay(0.01);
 	}
