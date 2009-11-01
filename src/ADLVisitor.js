a5c0bfedb713f0c89ab91be5d0b59947d19f3278
Christophe VG <xtof@pumpkin.local>
2009-11-01 11:00:02 +0100

    improved shape error testing

--- src/ADLVisitor.js
+++ src/ADLVisitor.js
@@ -11,21 +11,23 @@ Canvas2D.ADLVisitor = Class.extend( {
       return parent;
     } else if( Canvas2D.shapes.get(constructType) ) {
       var shape = Canvas2D.shapes.get(constructType).from(construct, parent);
-      if( shape.errors ) {
-        shape.errors.iterate( function( error ) {
-          this.errors.push( error );
-        }.scope(this) );
-      } else if( shape ) {
-        var left, top;
-        if( construct.annotation ) {    
-          var pos = construct.annotation.data.split(",");
-          left = parseInt(pos[0]);
-          top  = parseInt(pos[1]);
-          parent.at(left,top).add( shape );
-        } else {
-          parent.add( shape );
+      if( shape ) {
+        if( shape.errors ) {
+          shape.errors.iterate( function( error ) {
+            this.errors.push( error );
+          }.scope(this) );
+        } else if( shape ) {
+          var left, top;
+          if( construct.annotation ) {
+            var pos = construct.annotation.data.split(",");
+            left = parseInt(pos[0]);
+            top  = parseInt(pos[1]);
+            parent.at(left,top).add( shape );
+          } else {
+            parent.add( shape );
+          }
+          construct.childrenAccept(this, shape);
         }
-        construct.childrenAccept(this, shape);
       }
       return construct;
     } else {
