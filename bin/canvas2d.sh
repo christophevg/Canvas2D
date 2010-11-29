3a43dd1b366bda07d513cc5e45bda3f11df243ec
Christophe VG <xtof@pumpkin.local>
2010-11-29 22:59:06 +0100

    added support to run single unit tests using the Canvas2D Shell

--- bin/canvas2d.sh
+++ bin/canvas2d.sh
@@ -6,8 +6,13 @@ else
   SCRIPT=$1
 fi
 
+START_RUNNER=""
+if [[ ${SCRIPT} =~ "t/test" ]]; then
+  START_RUNNER="-f t/startTestRunner.js"
+fi
+
 java -jar lib/common.make/js.jar -w -debug  -opt -1 \
      -e "load( 'lib/common.make/env.rhino.js' );" \
      -e "load( 'build/Canvas2D.cli.js' );" \
      -e "print( 'Canvas2D-Shell' );" \
-     -f ${SCRIPT}
+     -f ${SCRIPT} ${START_RUNNER}
