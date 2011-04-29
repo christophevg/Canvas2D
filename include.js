e39f132e2e1ff83c4e9d17fa41bcf4c92d8c7400
Christophe VG <xtof@astroboy.local>
2011-04-30 00:45:22 +0200

    fixed issue #5: a build-less useable respository after introducing the new include.js pattern, the repository can now be checked out and used as is.

--- include.js
+++ include.js
@@ -1,6 +1,34 @@
-var scripts = [ 
-    "lib/ProtoJS/build/ProtoJS.js",
-    "lib/ADL/build/ADL.shared.js",
+(function(project, scripts) {
+	var cwd = (function cwd() {
+		var scripts = document.getElementsByTagName('script');
+		var re = new RegExp( project + "\/include\.js$");
+		for( var i in scripts) {
+			if( scripts[i].src && scripts[i].src.match( re ) ) {
+				return scripts[i].src.replace(/(.*)include\.js$/, '$1');
+			}
+		}
+	})();
+
+	var include = function include(url) {
+		document.writeln( "<script type=\"text/javascript\" " + 
+											"        src=\"" + url + "\"></scr" + "ipt>" );
+	}
+
+	for( var i=0; i<scripts.length; i++ ) {
+		include( cwd + scripts[i]);
+	}
+})
+(
+	
+// The name of the project is needed to select the correct include.js file.
+// When reusing projects with nested include files this might be required.
+"Canvas2D",
+
+// The following list are the separate Javascript files, in order, to be 
+// loaded. They are relative to this include file.
+[ 
+    "lib/ProtoJS/include.js",
+    "lib/ADL/include.js",
     "lib/excanvas.js",
     "lib/canvastext.js",
     "lib/tabber.js",
@@ -52,20 +80,6 @@ var scripts = [
     "src/plugins/KeyboardStatus.js",
     "src/plugins/Splash.js",
     "src/plugins/Inspector.js"
-];
+]
 
-function addScript(url) {
-  document.writeln( "<script type=\"text/javascript\" src=\"" + url + "\"></script>" );
-}
-
-function loadScripts(prefix) {
-  for( var i=0; i<scripts.length; i++ ) {
-    addScript(prefix + scripts[i]);
-  }
-}
-
-if( typeof window.loadingPrefix == "undefined" ) {
-  window.loadingPrefix = "../../";
-}
-
-loadScripts(window.loadingPrefix);
+);
